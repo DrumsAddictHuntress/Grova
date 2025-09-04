42d4f983e2a9a77ef27854be81cdd3e36788d85f
DrumsAddictHuntress <darma.marsonia2@gmail.com>
2025-09-04 18:45:07 -0400

    Fix compost arrival input, normalize number fields, add SECRET_KEY to config

--- config.py
+++ config.py
@@ -1,15 +1,20 @@
 
-import os
+import os, secrets
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
+
+
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY") or "dev-" + secrets.token_hex(32)
+
+
+db = SQLAlchemy(app)
 CORS(app)
+
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "sqlite:///grova.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-db = SQLAlchemy(app)
 migrate = Migrate(app, db)
